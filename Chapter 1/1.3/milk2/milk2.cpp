a714218848ef88355fc402efec82d3fca2884cec
jeffq123456 <jeffrey.qian@live.com>
2018-10-07 20:40:32 -0500

    included .vs

---
+++ Chapter 1/1.3/milk2/milk2.cpp
@@ -0,0 +1,16 @@
+/*
+ID: jeffrey66
+TASK: milk2
+LANG: C++
+*/
+
+#include <iostream>
+#include <fstream>
+
+
+int main(){
+    ifstream
+
+
+    return 0;
+}
---
+++ milk2/milk2.cpp
@@ -0,0 +1,18 @@
+/*
+ID: jeffrey66
+TASK: milk2
+LANG: C++
+*/
+
+#include <iostream>
+#include <fstream>
+
+
+using namespace std;
+
+
+
+int main(){
+    cout << "hello world"<<endl;
+    return 0;
+}
