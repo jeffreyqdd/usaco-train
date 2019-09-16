994a1d374b410ffe767c9196ae51b4a08e32fcdd
BuildTools <unconfigured@null.spigotmc.org>
2019-09-15 22:14:12 -0500

    unfinished..

--- dream/dream.cpp
+++ dream/dream.cpp
@@ -1,128 +1,118 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <queue>
+#include <vector>
 using namespace std;
-//u,r,d,l
-int dc[] = {0,1,0,-1};
-int dr[] = {-1,0,1,0};
 
-struct State
+//u,l,d,r
+int dr[] = {-1, 0, 1, 0};
+int dc[] = {0, -1, 0, 1};
+
+struct Node
 {
     int r;
     int c;
+    int dir;
     bool smell;
 
-    void unpack(int num)
-    {
-        r = num / 10000;
-        c = (num / 10) % 1000;
-        smell = num % 10;
-    }
     int pack()
     {
-        return (10 * c) + (10000 * r) + (smell? 1 : 0);
+        return (smell ? 1 : 0) + 2 * (dir + 1) + 10 * c + 10000 * r;
     }
 };
 
-int dijkstra(vector<vector<int>> grid, int N, int M)
+
+int get_cell(vector<vector<int>> a, int r, int c)
 {
-    vector<int> distance(10000000,-1); 
-    queue<State> q;
-
-    q.push({0,0,false});
     
-    
-    distance[q.front().pack()] = 0;
-
-
-    while(!q.empty())
-    {
-        State current = q.front(); q.pop();
-        //check if there
-        if(current.r == N - 1 && current.c == M-1)
-            return distance[current.pack()];
-
-        for(int i = 0; i < 4; i++)
-        {
-            //unpack
-            int currentWeight = distance[current.pack()];
-            int newR = current.r + dr[i], newC = current.c + dc[i];
-
-            cout << current.r << "," << current.c << "-->" << grid[current.r][current.c] << endl;
-            //check boundaries
-            if(newR < 0 || newR >= N || newC < 0 || newC >= M)
-                continue;
-
-            //red
-            if(grid[newR][newC] == 0)
-                continue;
-
-            //pink, orange, blue
-            if(grid[newR][newC] == 1 || grid[newR][newC] == 2 || grid[newR][newC] == 3)
-            {
-                //new node
-                State nNode;
-                if(grid[newR][newC] == 2) nNode = {newR, newC, true};
-                else nNode = {newR, newC, current.smell};
-
-                //if not smell like oranges
-                if(grid[newR][newC] == 3 && !current.smell)
-                    continue;
-
-                //if new weight is better than old, or unvisited
-                if((currentWeight + 1 < distance[nNode.pack()]) || distance[nNode.pack()] == -1)
-                {
-                    distance[nNode.pack()] = currentWeight + 1;
-                    q.push(nNode);
-                }
-            }
-
-            //purple
-            if(grid[newR][newC] == 4)
-            {
-                //slide: while in bounds and still slide
-                State nNode = {newR, newC, false};
-
-                
-                if(!((currentWeight + 1 < distance[nNode.pack()]) || distance[nNode.pack()] == -1))
-                    continue;
-                cout << "here\n";
-                while((newR < 0 || newR >= N || newC < 0 || newC >= M) && grid[nNode.r][nNode.c] == 4)
-                {
-                    distance[nNode.pack()] = currentWeight + 1;
-                    nNode.r += dr[i];
-                    nNode.c += dc[i];
-                    //
-                    
-
-                }
-                distance[nNode.pack()] = currentWeight + 1;
-
-                q.push(nNode); 
-            }
-            
-
-        }
-    }
-    return -1;
-
+    if(r < 0 || r >= a.size() || c < 0 || c >= a[0].size())
+        return 0;
+    return a[r][c];
 }
-
 int main()
 {
     ifstream fin("dream.in");
     ofstream fout("dream.out");
 
-    int N,M; fin >> N >> M;
-    vector<vector<int>> grid(N, vector<int> (M));
+    int N, M; fin >> N >> M;
 
+    vector<vector<int>> grid(N, vector<int>(M));
+    
     for(int r = 0; r < N; r++)
         for(int c = 0; c < M; c++)
             fin >> grid[r][c];
-    fin.close();
 
-    cout << dijkstra(grid, N, M) << endl;
+    for(int r = 0; r < N; r++)
+    {
+        for(int c = 0; c < M; c++)
+        {
+            cout << grid[r][c] << " ";
+        }
+        cout << endl;
+    }
+    
+    queue<Node> q;
+    vector<int> dist(10000000,-1);
+
+    q.push({0,0,-1,false});
+    dist[0] = 0;
+
+    while(!q.empty())
+    {   
+        cout << q.size() << ": ";
+
+        Node cNode = q.front(); q.pop();
+
+        cout << cNode.r << " " << cNode.c << "-->" << cNode.dir << endl;
+
+        if(cNode.r == N - 1 && cNode.c == M - 1)
+        {
+            cout << "made it" << endl;
+            fout << dist[cNode.pack()] << endl;
+            return 0;
+        }
+
+
+        //purple
+        if(get_cell(grid, cNode.r, cNode.c) == 4 && cNode.dir != -1)
+        {
+            int next = get_cell(grid, cNode.r + dr[cNode.dir], cNode.c + dc[cNode.dir]);
+            if(next != 0 && next != 3)
+            {
+                cout << "considering\n";
+                Node nNode = {cNode.r + dr[cNode.dir], cNode.c + dc[cNode.c], cNode.dir, next == 2};
+                cout << dist[nNode.pack()] << endl;
+                if(dist[nNode.pack()] != -1)
+                    continue;
+
+                cout << "weee\n" << endl; 
+                dist[nNode.pack()] = dist[cNode.pack()] + 1;
+                q.push(nNode);
+            } 
+        }
+
+        for(int i = 0; i < 4; i++)
+        {
+            int next = get_cell(grid, cNode.r + dr[i], cNode.c + dc[i]);
+
+            //red or blue w/o smell
+            if(next == 0 || (next == 3 && !cNode.smell))
+                continue;
+
+            Node nNode = {cNode.r + dr[i], cNode.c + dc[i], i, next == 2 ? true : next == 4 ? false : cNode.smell};
+
+            if(dist[nNode.pack()] != -1)
+                continue;
+
+            dist[nNode.pack()] = dist[cNode.pack()] + 1;
+            q.push(nNode);
+        }
+    }
+    cout << "here\n";
+    fout << -1 << endl;
+
+    fin.close();
+    fout.close();
 
     return 0;
 }
